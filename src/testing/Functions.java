548faf0631e334ce9df9649d1fcb86bc7f1fd2e1
sm praneeth kumar <188424121+smpraneethkumar@users.noreply.github.com>
2026-03-10 22:51:38 +0530

    added matter for for loop so that your can understand what is #FORLOOP

--- src/testing/Functions.java
+++ src/testing/Functions.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class Functions {
 
     public static  void forLoop(){
+        System.out.println("-> FOR LOOP:- If you want to execute certain things for certain times ");
         Scanner scan = new Scanner(System.in);
+
         System.out.println("enter your starting index here:");
         int start = scan.nextInt();
         System.out.println("enter your ending index here:");
@@ -13,6 +15,7 @@ public class Functions {
 
         int  i =0;
         while (i<end){
+            System.out.println("Enter the running index:");
         int userValue = scan.nextInt();
         System.out.println("for(i = " +userValue+"; i < "+end+"; i++) {");
         System.out.println("Hello World!");
@@ -36,10 +39,14 @@ public class Functions {
         System.out.println("enter your ending index here:");
         int end = scan.nextInt();
 
+while (start< end) {
 
-        System.out.println("while("+start+" < "+end+"){");
-        System.out.println("Hello World!");
-        System.out.println("i++; //HERE THE i VALUE WILL BE INCREMENTED");
-        System.out.println("}");
+
+    System.out.println("while(" + start + " < " + end + "){");
+    System.out.println("Hello World!");
+    System.out.println("i++; //HERE THE i VALUE WILL BE INCREMENTED");
+    System.out.println("}");
+start++;
+}
     }
 }
